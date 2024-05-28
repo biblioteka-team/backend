e6351104632efed0e2207710d4c7d15c05accb48
Mary Brovarska <marybrovarska@Marys-MacBook-Air.local>
2024-05-27 20:06:55 -0400

    function that returns books changed, subdocuments populated

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -4,27 +4,24 @@ const catchAsync = require("../utils/catchAsync");
 
 exports.getNewAndSalesAndBestsellerBooks = catchAsync(async (req, res, next) => {
     try {
-    // const date = new Date();
-    // date.setDate(date.getDate() - 2);
-    // date.toDateString();
     const [newBooks, salesBooks, bestsellerBooks] = await Promise.all([
         //get new books by date
-        // Book.find({created: {$gte: date, 
-        //     $lt: new Date()}}).populate("price_id"),
-        Book.find().sort({ created: -1 }).populate("price_id"),
+        Book.find().sort({ created: -1 }).populate("price_id").populate("author_id")
+        .populate("publisher_id").populate("language_id").populate("category_id"),
+
         //get books with discounted price
-        Price.find({discounted_price: { $gt: 0.0}}).populate("book_id"),
-        //get random bestsellers
-        Book.aggregate([
-            { $sample: { size: 4 } },
-            { $lookup: {
-                from: "prices",
-                localField: "_id",
-                foreignField: "book_id",
-                as: "price"
-            }}
-        ])
-      ]);
+       Price.find({discounted_price: { $gt: 0.0}}).populate({path: "book_id", 
+            populate : [
+                { path: "author_id"},
+                { path: "publisher_id"},
+                { path: "language_id"},
+                { path: "category_id"}
+            ]
+        }),
+
+        Book.find().limit(4).populate("price_id").populate("author_id")
+        .populate("publisher_id").populate("language_id").populate("category_id"),
+     ]);
       res.json({
         newBooks: newBooks,
         salesBooks: salesBooks,
