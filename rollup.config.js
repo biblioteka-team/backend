183c189e7dc53f98ea0b1eaff05b653690420124
Mary Brovarska <marybrovarska@Marys-MacBook-Air.local>
2024-07-22 16:31:58 -0400

    rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,4 @@
 export default {
     input: './server.js',
-    output: {
-        file: './build/bundle.min.js',
-        format: 'iife',
-        name: 'bundle'
-    },
     plugins: [json()],
 }
